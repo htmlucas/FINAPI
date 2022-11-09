975d9249e4bcf7b5a259c89384dbaa01c28155c7
Lucas Martins de Freitas <lucasmartinsde@gmail.com>
2022-11-09 15:36:09 -0300

    created middleware, e more functions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const { response } = require("express");
 const express = require("express");
 const { v4: uuidv4 } = require("uuid");
 
@@ -12,6 +13,25 @@ NAME - STRING
 ID - UUID
 STATEMENT - []
 */
+
+//middleware
+function verifyIfExistsAccountCPF(request, response, next){
+
+    const { cpf } = request.headers;
+
+    const customer = customers.find(customer => customer.cpf === cpf);
+
+    if(!customer){
+        return response.status(400).json({ error: "Customer not found" });
+    }
+
+    request.customer = customer;
+
+    return next();
+}
+
+//fim do middleware
+
 app.post("/account", (request, response) => {
     const {cpf, name} = request.body;
 
@@ -30,6 +50,30 @@ app.post("/account", (request, response) => {
         statement: []
     });
     
+    return response.status(201).send();
+});
+
+//app.use(verifyIfExistsAccountCPF);  é para quando todas as rotas precisam usar um middleware ai ja vale pra todas
+
+app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {    
+    const { customer } = request;
+    return response.json(customer.statement);
+});
+
+app.post("/deposit", verifyIfExistsAccountCPF,(request, response) => {
+    const { description, amount } = request.body;
+
+    const { customer } = request;
+
+    const statementOperation = {
+        description,
+        amount,
+        created_at: new Date(),
+        type: "credit"
+    }
+
+    customer.statement.push(statementOperation);
+
     return response.status(201).send();
 });
 
